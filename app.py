873b35435c4908d776a196a6bef70f6cbf6686df
SenaS0S <sena552josh@gmail.com>
2025-09-19 04:43:00 +0000

    ident

--- app.py
+++ app.py
@@ -216,4 +216,4 @@ def upload_file():
 # ==============================================================================
 if __name__ == '__main__':
     init_db()
-        app.run(host='0.0.0.0', port=5000, debug=True)
+app.run(host='0.0.0.0', port=5000, debug=True)
